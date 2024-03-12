0f37341dc44c08a457b4cf33643a0bad4132090d
Caitano <josias.m.caitano@accenture.com>
2024-03-12 11:06:44 -0300

    feature: add DevalueValueV2

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -1,5 +1,6 @@
 /** decorators */
 export * from "./decorators/default-value.decorator";
+export * from "./decorators/default-value.v2.decorator";
 export * from "./decorators/log-execution-time.decorator";
 
 /** utils */
