516faa262544c46c7ec5284986fad42796a78a0a
Caitano <josias.m.caitano@accenture.com>
2024-03-15 22:13:31 -0300

    refactor: change path log-execution

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -1,7 +1,7 @@
 /** decorators */
 export * from "./decorators/default-value.decorator";
 export * from "./decorators/default-value.v2.decorator";
-export * from "./decorators/log-execution-time.decorator";
+export * from "./decorators/log-execution/log-execution-time.decorator";
 
 /** utils */
 export * from "./utils/builder/builder.util";
